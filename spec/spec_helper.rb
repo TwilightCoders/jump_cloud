914d026114891681bfa5280136184ee52be5edbe
Dale Stevens <dale@getnotion.com>
2018-05-15 09:53:52 -0600

    Don't puts config in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,8 +18,6 @@ JumpCloud::Agent.install
   sleep 1
 end
 
-puts JumpCloud::Agent.config
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
